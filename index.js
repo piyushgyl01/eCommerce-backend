4704f7aa559dcaf93592ea3098bca40c8f6e0a30
piyush.gyl <168422795+piyushgyl01@users.noreply.github.com>
2024-12-11 22:21:45 +0530

    Update cors origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true,
   optionSuccessStatus: 200,
 };
